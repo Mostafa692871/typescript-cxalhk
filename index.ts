e961e2a5c095924ccb3064912527536e3a63ad87
Mostafa692871 <mostafahero556@gmail.com>
2022-07-30 06:45:06 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -4,3 +4,9 @@ import './style.css';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+let num1: number;
+let num2: number;
+num1 = 123;
+num2 = 457;
+
+console.log['num1'];
